69f36801874128def8ec3edfefdaa085d48b75db
needo <malcolm@joker.lan>
2014-06-25 18:34:02 -0500

    Open up initial root mysql connections

--- firstrun.sh
+++ firstrun.sh
@@ -5,4 +5,7 @@ if [ -f /db/mysql/user.MYD ]; then
   echo "database exists"
 else
   /usr/bin/mysql_install_db --datadir=/db
+  mysql -u root -e \
+      "GRANT ALL PRIVILEGES ON *.* TO 'root'@'*';"
+  mysqladmin -u root shutdown
 fi
