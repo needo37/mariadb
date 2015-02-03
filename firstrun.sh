d957b89590ec3fb7e6e1e7a8ace4be5bafe1aec7
sparklyballs <sparklyballs@gmail.com>
2015-02-03 10:34:12 +0000

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,1 +1,9 @@
-blah
+#!/bin/bash
+if [ -f "/db/my.cnf" ]; then
+echo "applying latest my.cnf"
+/bin/cp -f /db/my.cnf /etc/mysql/my.cnf
+else
+echo "copying initial my.cnf from root dir"
+/bin/cp -f /root/my.cnf /db/my.cnf
+/bin/cp -f /db/my.cnf /etc/mysql/my.cnf
+fi
