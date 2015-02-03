f12081d166e68fc1240b85fbf5938e19108a0fa0
sparklyballs <sparklyballs@gmail.com>
2015-02-03 10:33:51 +0000

    Update mariadb.sh

--- mariadb.sh
+++ mariadb.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-if [ -f "/db/my.cnf" ]; then
-echo "applying latest my.cnf"
-/bin/cp -f /db/my.cnf /etc/mysql/my.cnf
-else
-echo "copying initial my.cnf from root dir"
-/bin/cp -f /root/my.cnf /db/my.cnf
-/bin/cp -f /db/my.cnf /etc/mysql/my.cnf
-fi
-
 start_mysql(){
     /usr/bin/mysqld_safe --datadir=/db > /dev/null 2>&1 &
     RET=1
